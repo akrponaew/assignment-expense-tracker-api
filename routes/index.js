7e2afbddc33c009e6630a595d6e9f65142823ce2
Akrapon Panyawattanakit <arp@Akrapons-MacBook-Pro.local>
2020-06-07 13:56:01 +0700

    heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Change title' });
+  res.render('index', { title: 'Change titlesss' });
 });
 
 module.exports = router;
