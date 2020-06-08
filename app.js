86f2e0fcd8c4a62d7ac13ca1ad7620759af5be4c
Akrapon Panyawattanakit <arp@Akrapons-MacBook-Pro.local>
2020-06-08 20:25:43 +0700

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors')
 
 mongoose.connect('mongodb+srv://akrapon:panyawattanakit@dbserver-iibnu.gcp.mongodb.net/dbserver1')
 // mongoose.connect('mongodb+srv://akrapon:panyawattanakit@dbserver-iibnu.gcp.mongodb.net/dbserver1?retryWrites=true&w=majority')
@@ -18,6 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
